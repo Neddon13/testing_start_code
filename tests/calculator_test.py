821205b7b0efa1b3d201037c8c7bd6cabb0a27ac
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-10-26 21:14:51 +0100

    second commit

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,6 +1,16 @@
 import unittest
-from src.calculator import add, divide, mulitply, subtract
+from src.calculator import add, divide, multiply, subtract
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        pass
+        self.assertEqual(5, add(2,3))
+
+    def test_subtract(self):
+        self.assertEqual(3, subtract(10 ,7))
+
+    def test_divide(self):
+        self.assertEqual(6, divide(12, 2))
+
+    def test_multiply(self):
+        self.assertEqual(10, multiply(5, 2))
+        
